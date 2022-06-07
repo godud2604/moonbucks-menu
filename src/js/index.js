adea6cd245c84a6dc5d3238024e974ba231064b4
godud2604 <ees238@naver.com>
2022-06-07 20:04:12 +0900

    Edit the menu in the prompt by clicking the edit button

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,17 @@
 const $ = (selector) => document.querySelector(selector);
 
 function App() {
+  $('#espresso-menu-list').addEventListener('click', (e) => {
+    if (e.target.classList.contains('menu-edit-button')) {
+      const $menuName = e.target.closest('li').querySelector('.menu-name');
+
+      const menuEdit = prompt('메뉴명을 수정하세요.', $menuName.innerText);
+      if (menuEdit !== null) {
+        $menuName.innerText = menuEdit;
+      }
+    }
+  });
+
   $('#espresso-menu-form').addEventListener('submit', (e) => {
     e.preventDefault();
   });
