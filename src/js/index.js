f95a97dcf6c08c3aecff3957506d7c023f23948b
godud2604 <ees238@naver.com>
2022-06-08 17:15:19 +0900

    Refactoring step 1

--- src/js/index.js
+++ src/js/index.js
@@ -5,28 +5,6 @@ function App() {
     const menuCount = $('#espresso-menu-list').querySelectorAll('li').length;
     $('.menu-count').innerText = `총 ${menuCount} 개`;
   };
-  $('#espresso-menu-list').addEventListener('click', (e) => {
-    if (e.target.classList.contains('menu-edit-button')) {
-      const $menuName = e.target.closest('li').querySelector('.menu-name');
-
-      const menuEdit = prompt('메뉴명을 수정하세요.', $menuName.innerText);
-      if (menuEdit !== null) {
-        $menuName.innerText = menuEdit;
-      }
-    }
-    if (e.target.classList.contains('menu-remove-button')) {
-      const menuRemove = confirm('삭제 하시겠습니까?');
-      if (menuRemove) {
-        $('#espresso-menu-list').removeChild(e.target.closest('li'));
-
-        updateMenuCount();
-      }
-    }
-  });
-
-  $('#espresso-menu-form').addEventListener('submit', (e) => {
-    e.preventDefault();
-  });
 
   const addMenuName = () => {
     if ($('#espresso-menu-name').value === '') {
@@ -62,12 +40,41 @@ function App() {
     $('#espresso-menu-name').value = '';
   };
 
-  $('#espresso-menu-name').addEventListener('keypress', (e) => {
-    if (e.key !== 'Enter') return;
-    addMenuName();
+  const updateMenuName = (e) => {
+    const $menuName = e.target.closest('li').querySelector('.menu-name');
+
+    const menuEdit = prompt('메뉴명을 수정하세요.', $menuName.innerText);
+    if (menuEdit !== null) {
+      $menuName.innerText = menuEdit;
+    }
+  };
+
+  const removeMenuName = (e) => {
+    const menuRemove = confirm('삭제 하시겠습니까?');
+    if (menuRemove) {
+      $('#espresso-menu-list').removeChild(e.target.closest('li'));
+
+      updateMenuCount();
+    }
+  };
+
+  $('#espresso-menu-list').addEventListener('click', (e) => {
+    if (e.target.classList.contains('menu-edit-button')) {
+      updateMenuName(e);
+    }
+    if (e.target.classList.contains('menu-remove-button')) {
+      removeMenuName(e);
+    }
   });
 
-  $('#espresso-menu-submit-button').addEventListener('click', () => {
+  $('#espresso-menu-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+  });
+
+  $('#espresso-menu-submit-button').addEventListener('click', addMenuName);
+
+  $('#espresso-menu-name').addEventListener('keypress', (e) => {
+    if (e.key !== 'Enter') return;
     addMenuName();
   });
 }
